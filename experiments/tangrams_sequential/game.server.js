b5741d98d20d5134af1dc79348efc4b553fa8c00
hawkrobe <rxdh@stanford.edu>
2016-12-06 00:05:09 -0800

    Update server-side event logic

--- experiments/tangrams_sequential/game.server.js
+++ experiments/tangrams_sequential/game.server.js
@@ -27,48 +27,15 @@ var onMessage = function(client,message) {
   var target = gc.get_player(client.userid);
   var others = gc.get_others(client.userid);
 
-
   switch(message_type) {
     
-  case 'dropObj' :
-    // parse the message
-    var objIndex = message_parts[1];
-    var swapIndex = message_parts[2];
-    var objTrueX = message_parts[3];
-    var objTrueY = message_parts[4];
-    var swapObjTrueX = message_parts[5];
-    var swapObjTrueY = message_parts[6];
-    var objBox = message_parts[7];
-    var swapObjBox = message_parts[8];
-
-
-    // Update the local copy to match the new positions of these items!
-    gc.trialInfo.currStim[objIndex].matcherCoords.trueX = objTrueX;
-    gc.trialInfo.currStim[objIndex].matcherCoords.trueY = objTrueY;
-    gc.trialInfo.currStim[objIndex].matcherCoords.box = objBox;
-    gc.trialInfo.currStim[swapIndex].matcherCoords.trueX = swapObjTrueY;
-    gc.trialInfo.currStim[swapIndex].matcherCoords.trueY = swapObjTrueY;
-    gc.trialInfo.currStim[swapIndex].matcherCoords.box = swapObjBox;
-
-    //get the gridX and gridY values
-    var objCell = gc.getCellFromPixel(objTrueX, objTrueY);
-    var swapObjCell = gc.getCellFromPixel(swapObjTrueX, swapObjTrueY);
-
-    //Update local copy with correct gridX and gridY values
-    gc.trialInfo.currStim[objIndex].matcherCoords.gridX = objCell[0];
-    gc.trialInfo.currStim[objIndex].matcherCoords.gridY = objCell[1];
-    gc.trialInfo.currStim[swapIndex].matcherCoords.gridX = swapObjCell[0];
-    gc.trialInfo.currStim[swapIndex].matcherCoords.gridY = swapObjCell[1];  
-
-    writeData(client, "dropObj", message_parts);
-    break;
-  
-  case 'advanceRound' :
+  case 'clickedObj' :
     var score = gc.game_score(gc.trialInfo.currStim);
     gc.data.totalScore += score;
 
     var boxLocations = message_parts[1];
-    writeData(client, "finalBoard", message_parts);
+    writeData(client, "clickedObj", message_parts);
+
     _.map(all, function(p){
       p.player.instance.emit( 'newRoundUpdate', {user: client.userid, score: score});});
     gc.newRound();
@@ -104,28 +71,22 @@ var writeData = function(client, type, message_parts) {
   var score = gc.game_score(gc.trialInfo.currStim);  
   var line;
   switch(type) {
-    case "dropObj" :
-      var dropObjBox = message_parts[7];
-      var dropObjName = message_parts[9];
-      line = (id + ',' + Date.now() + ',' + roundNum + ',' + score + ',' +
-	      dropObjName + ',' + dropObjBox + '\n');
-      break;
+  case "clickedObj" :
+    // parse the message
+    var objName = message_parts[1];
+    var objBox = message_parts[2];
+    var objTrueX = message_parts[3];
+    var objTrueY = message_parts[4];
+    line = [id, Date.now(), roundNum, score, objName, objBox];
+    break;
 
-    case "message" :
-      var msg = message_parts[1].replace('~~~','.');
-      line = (id + ',' + Date.now() + ',' + roundNum + ',' +
-	      client.role + ',"' + msg + '"\n');
-      break;
-
-    case "finalBoard" :
-      var submittedBoard = message_parts[1];
-      var trueBoard = gc.getBoxLocs(gc.trialInfo.currStim, "director");
-      line = (id + ',' + Date.now() + ',' + roundNum + ',' +
-	      submittedBoard + ',' + trueBoard + ',' + score + '\n');
-      break;
+  case "message" :
+    var msg = message_parts[1].replace('~~~','.');
+    line = [id, Date.now(), roundNum, client.role, msg];
+    break;
   }
-  console.log(type + ":" + line);
-  gc.streams[type].write(line, function (err) {if(err) throw err;});
+  console.log(type + ":" + line.join(','));
+  gc.streams[type].write(line.join(','), function (err) {if(err) throw err;});
 };
 
 var startGame = function(game, player) {
