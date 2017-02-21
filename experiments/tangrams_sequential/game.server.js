a51a2ff69b327e8d9b73cba29e1442267a3d1ea9
hawkrobe <rxdh@stanford.edu>
2017-02-20 18:51:11 -0800

    Fix data streams; show feedback

--- experiments/tangrams_sequential/game.server.js
+++ experiments/tangrams_sequential/game.server.js
@@ -30,15 +30,16 @@ var onMessage = function(client,message) {
   switch(message_type) {
     
   case 'clickedObj' :
-    var score = gc.game_score(gc.trialInfo.currStim);
-    gc.data.totalScore += score;
-
-    var boxLocations = message_parts[1];
     writeData(client, "clickedObj", message_parts);
-
-    _.map(all, function(p){
-      p.player.instance.emit( 'newRoundUpdate', {user: client.userid, score: score});});
-    gc.newRound();
+    others[0].player.instance.send('s.feedback.' + message_parts[1]);
+    target.instance.send('s.feedback.' + message_parts[1]);
+    // TODO: trigger feedback
+    setTimeout(function() {
+      _.forEach(all, function(p){
+	p.player.instance.emit( 'newRoundUpdate', {user: client.userid});
+      });
+      gc.newRound();
+    }, 3000);
     break;
   
   case 'playerTyping' :
@@ -75,8 +76,6 @@ var writeData = function(client, type, message_parts) {
     // parse the message
     var objName = message_parts[1];
     var objBox = message_parts[2];
-    var objTrueX = message_parts[3];
-    var objTrueY = message_parts[4];
     line = [id, Date.now(), roundNum, score, objName, objBox];
     break;
 
@@ -86,7 +85,7 @@ var writeData = function(client, type, message_parts) {
     break;
   }
   console.log(type + ":" + line.join(','));
-  gc.streams[type].write(line.join(','), function (err) {if(err) throw err;});
+  gc.streams[type].write(line.join(',') + "\n", function (err) {if(err) throw err;});
 };
 
 var startGame = function(game, player) {
@@ -96,12 +95,8 @@ var startGame = function(game, player) {
   var dataFileName = startTime + "_" + game.id;
   utils.establishStream(game, "message", dataFileName,
 		       "gameid,time,roundNum,sender,contents\n");
-  utils.establishStream(game, "dropObj", dataFileName,
-		       "gameid,time,roundNum,score,name,draggedTo\n");
-  utils.establishStream(game, "finalBoard", dataFileName,
-		       "gameid,time,roundNum,subA,subB,subC,subD,subE,subF,subG," +
-                       "subH,subI,subJ,subK,subL,trueA,trueB,trueC,trueD,trueE," + 
-                       "trueF,trueG,trueH,trueI,trueJ,trueK,trueL,score\n");
+  utils.establishStream(game, "clickedObj", dataFileName,
+		       "gameid,time,roundNum,score,objName,objBox\n");
   game.newRound();
   game.server_send_update();
 };
