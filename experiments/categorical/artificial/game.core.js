8c76567b6b9f3d2d436a4b4003ec291493d3f461
hawkrobe <rxdh@stanford.edu>
2017-06-27 11:59:17 -0700

    Made artificial-language basic-level

--- experiments/categorical/artificial/game.core.js
+++ experiments/categorical/artificial/game.core.js
@@ -22,16 +22,11 @@ if( typeof _ === 'undefined' ) {
     _ = require('underscore');
     utils  = require(__base + 'sharedUtils/sharedUtils.js');
     assert = require('assert');
-    jsonfile = require('jsonfile')
-
-    TRIALS_OBJECT_FROM_JSON = _.shuffle(require("./trials.json"));
-    console.log(TRIALS_OBJECT_FROM_JSON);
-    assert(_.isArray(TRIALS_OBJECT_FROM_JSON) && TRIALS_OBJECT_FROM_JSON.length == 50);
   }
   else throw 'mymodule requires underscore, see http://underscorejs.org';
 }
 
-var WORLD_HEIGHT = 400;
+var WORLD_HEIGHT = 600;
 var WORLD_WIDTH = 600;
 
 var game_core = function(options){
@@ -46,8 +41,8 @@ var game_core = function(options){
   };
 
   //Dimensions of world in pixels and numberof cells to be divided into;
-  this.numHorizontalCells = 3;
-  this.numVerticalCells = 1;
+  this.numHorizontalCells = 2;
+  this.numVerticalCells = 2;
   this.cellDimensions = {height : 300, width : 300}; // in pixels
   this.cellPadding = 0;
   this.world = {
@@ -58,7 +53,7 @@ var game_core = function(options){
   this.roundNum = -1;
 
   // How many rounds do we want people to complete?
-  this.numRounds = 30;
+  this.numRounds = 60;
 
   // This will be populated with the tangram set
   this.trialInfo = {};
@@ -69,7 +64,8 @@ var game_core = function(options){
     this.id = options.id;
     this.expName = options.expName;
     this.player_count = options.player_count;
-    this.trialList = _.shuffle(TRIALS_OBJECT_FROM_JSON);
+    this.objects = require('./objects.json');
+    this.trialList = this.makeTrialList();
     this.data = {
       id : this.id,
       trials : [],
@@ -173,26 +169,6 @@ game_core.prototype.makeTrialList = function () {
   return(trialList);
 };
 
-// game_core.prototype.noisify = function(trialList) {
-//   var noise = 10;
-
-//   //TODO this is so slow.. even necessary?
-
-//   return _.map(trialList, function(world) {
-//     return _.mapObject(world, function(shape) {
-//       if (_.isString(shape)) return shape;
-
-//       return _.mapObject(shape, function (value) {
-
-//         var sign = _.sample([-1, 1]);
-//         var amount = Math.floor(_.random(0, 100) / noise);
-
-//         return value + (sign * amount);
-//       });
-//     });
-//   });
-// }
-
 game_core.prototype.server_send_update = function(){
   //Make a snapshot of the current state, for updating the clients
   var local_game = this;
@@ -221,113 +197,3 @@ game_core.prototype.server_send_update = function(){
     p.player.instance.emit( 'onserverupdate', state);});
 };
 
-game_core.prototype.sampleTrial = function() {
-  var options = {
-    //for rect (and point)
-    xMin: 50,
-    xMax: 600,
-    yMin: 50,
-    yMax: 400,
-    wMin: 50,
-    wMax: 350,
-    hMin: 50,
-    hMax: 350,
-
-    //circle
-    dMin: 100,
-    dMax: 100,
-
-    width: WORLD_WIDTH,
-    height: WORLD_HEIGHT
-  };
-
-  //the random functions handle bounds checking for us
-  var getRandomRect = function getRandomRect() {
-    return utils.randomRect(options);
-  }
-
-  var getRandomCircle = function getRandomCircle(world) {
-    var plazaOptions = _.clone(options);
-    var mode = _.random(1,3);
-
-    if (mode == 1) { //plaza somewhere near red
-      options.xMin = world.red.x;
-      options.xMax = world.red.x + world.red.w;
-      options.yMin = world.red.y;
-      options.yMax = world.red.y + world.red.h;
-
-    } else if (mode == 2) { //plaza somewhere near blue
-      options.xMin = world.blue.x;
-      options.xMax = world.blue.x + world.blue.w;
-      options.yMin = world.blue.y;
-      options.yMax = world.blue.y + world.blue.h;
-    }
-
-    return utils.randomCircle(plazaOptions);
-  }
-
-  var getRandomPoint = function getRandomPoint(world) {
-    var lilyOptions = _.clone(options);
-    var mode = _.random(1,3);
-
-    if (mode == 1) { //lily somewhere near red
-      options.xMin = world.red.x;
-      options.xMax = world.red.x + world.red.w;
-      options.yMin = world.red.y;
-      options.yMax = world.red.y + world.red.h;
-
-    } else if (mode == 2) { //lily somewhere near blue
-      options.xMin = world.blue.x;
-      options.xMax = world.blue.x + world.blue.w;
-      options.yMin = world.blue.y;
-      options.yMax = world.blue.y + world.blue.h;
-    } else if (mode == 3) { //lily somewhere near plaza
-      options.xMin = world.plaza.x;
-      options.xMax = world.plaza.x + world.plaza.w;
-      options.yMin = world.plaza.y;
-      options.yMax = world.plaza.y + world.plaza.h;
-    }
-
-    return utils.randomPoint(lilyOptions);
-  }
-
-  var world = {
-    red: getRandomRect(),
-    blue: getRandomRect(),
-  };
-
-  world.plaza = getRandomCircle(world);
-  world.lily = getRandomPoint(world);
-
-  if (!checkWorld(world, options)) {
-    //the world is invalid, so we just resample
-
-    return this.sampleTrial();
-  }
-
-  return world;
-};
-
-var checkWorld = function(world, options) {
-
-  //takes two rects with x, y, w, h parameters
-  var existsIntersection = function(object1, object2) {
-    //convert to bounding points
-    var convertToBounds = function(object) {
-      return {
-        x1: object.x,
-        x2: object.x + object.w,
-        y1: object.y,
-        y2: object.y + object.h
-      }
-    }
-
-    var o1 = convertToBounds(object1);
-    var o2 = convertToBounds(object2);
-
-    return ((o1.x1 >= o2.x1 && o1.x1 < o2.x2) || (o2.x1 >= o1.x1) && (o2.x1 < o1.x2)) &&
-           ((o1.y1 < o2.y2 && o1.y1 >= o2.y1) || (o2.y1 < o1.y2 && o2.y1 >= o1.y1))
-  }
-
-  return !existsIntersection(world.red, world.blue);
-};
