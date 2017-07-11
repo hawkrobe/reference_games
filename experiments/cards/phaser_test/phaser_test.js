aaf9fe3dab885fcaafde4f26720ad018ee07d679
Andrew Jong <andrewjong87@gmail.com>
2017-07-11 11:53:23 -0700

    Added end turn button

--- experiments/cards/phaser_test/phaser_test.js
+++ experiments/cards/phaser_test/phaser_test.js
@@ -3,7 +3,9 @@ const options = {
     gameHeight: 700,
     cardSheetWidth: 334,
     cardSheetHeight: 440,
-    cardScale: 0.3
+    cardScale: 0.3,
+    turnButtonWidth: 188,
+    turnButtonHeight: 46
 }
 
 window.onload = function () {
@@ -14,14 +16,16 @@ window.onload = function () {
 
 let playGame = function (game) { }
 let isMyTurn = false;
+let self = this;
 
 // horizontal gap between cards
-let cardGap = 120;
+const cardGap = 120;
 
 playGame.prototype = {
     preload: function () {
         game.load.spritesheet('cards', 'cards.png', options.cardSheetWidth, options.cardSheetHeight);
         game.load.image('cardback', 'cardback.png', options.cardSheetWidth, options.cardSheetHeight);
+        game.load.spritesheet('end-turn', 'end-turn-button.png', 188, 46);
     },
     create: function () {
         game.stage.backgroundColor = '#076324';
@@ -55,19 +59,27 @@ playGame.prototype = {
         const barWidth = game.world.width;
         const barHeight = 75;
         const barYOffset = game.world.height - barHeight;
-        bar.beginFill(0x000000, 0.2);
+        bar.beginFill('#000', 0.2);
         bar.drawRect(0, barYOffset, barWidth, barHeight);
 
-        const turnStyle = { font: 'bold 32px Arial', fill: '#FFF', boundsAlignH: 'center', boundsAlignV: 'middle' };
-        isPartner = isMyTurn ? '' : 'partner\'s '
-        turnText = game.add.text(0, 0, 'It\'s your ' + isPartner + 'turn.', turnStyle);
+        const turnTextStyle = { font: 'bold 32px Arial', fill: '#FFF', boundsAlignH: 'center', boundsAlignV: 'middle' };
+        let isPartner = isMyTurn ? '' : 'partner\'s '
+        turnText = game.add.text(0, 0, 'It\'s your ' + isPartner + 'turn.', turnTextStyle);
         turnText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
         turnText.setTextBounds(0, barYOffset, barWidth, barHeight);
+
+        // const button = game.add.button(game.world.width - options.turnButtonWidth, 
+        //                                 game.world.height - options.turnButtonHeight, 
+        //                                 'end-turn', this.nextTurn, 0, 1, 2, 3);
+        const button = game.add.button(game.world.centerX, 
+                                        game.world.centerY, 
+                                        'end-turn', this.nextTurn, this, 0, 1, 2);
+        button.anchor.set(0.5);
     },
     // startIndex = index in this.deck where hand should start
     // thisPlayer = true if this player, false if that player
     makeHand: function (startIndex, thisPlayer) {
-        let dy = thisPlayer ? -200 : 200;
+        let dy = thisPlayer ? 200 : -200;
         let hand = [0, 1, 2].map(i =>
             this.makeCard(startIndex + i, this.game.world.centerX + (i - 1) * cardGap, this.game.world.centerY + dy));
         return hand;
@@ -77,78 +89,97 @@ playGame.prototype = {
             this.makeCard(startIndex + i, this.game.world.centerX + (i - 1.5) * cardGap, this.game.world.centerY));
         return onTable;
     },
-    makeCard: function(cardIndex, x, y) {
-      // initialize card
-      let card = game.add.sprite(x, y, 'cards', this.deck[cardIndex]);
-      card.scale.set(options.cardScale);
-      card.anchor = new Phaser.Point(0.5,0.5);
+    makeCard: function (cardIndex, x, y) {
+        // initialize card
+        let card = game.add.sprite(x, y, 'cards', this.deck[cardIndex]);
+        card.scale.set(options.cardScale);
+        card.anchor = new Phaser.Point(0.5, 0.5);
 
-      // enable drag and drop
-      this.game.physics.arcade.enable(card);
-      card.inputEnabled = true;
-      card.input.enableDrag();
-      card.originalPosition = card.position.clone();
-      card.events.onDragStart.add(this.startDrag, this);
-      card.events.onDragStop.add(this.stopDrag, this);
+        // enable drag and drop
+        this.game.physics.arcade.enable(card);
+        card.inputEnabled = true;
+        card.input.enableDrag();
+        card.originalPosition = card.position.clone();
+        card.events.onDragStart.add(this.startDrag, this);
+        card.events.onDragStop.add(this.stopDrag, this);
 
-      return card;
+        return card;
     },
-    startDrag: function(card, pointer){
-      game.world.bringToTop(card);
-      card.tint = 0xFFFF00;
+    startDrag: function (card, pointer) {
+        game.world.bringToTop(card);
+        card.tint = 0xFFFF00;
     },
-    stopDrag: function(card, pointer){
-      // unhighlight dragged card
-      card.tint = 0xFFFFFF;
+    stopDrag: function (card, pointer) {
+        // unhighlight dragged card
+        card.tint = 0xFFFFFF;
 
-      _this = this
+        _this = this
 
-      // check for overlap in cards on table and within hand
-      for (i = 0; i < this.onTable.length; i++){
-        if(game.physics.arcade.overlap(this.onTable[i], card, this.swapCards)) {
-          console.log('overlap with card on table');
+        // check for overlap in cards on table and within hand
+        for (i = 0; i < this.onTable.length; i++) {
+            if (game.physics.arcade.overlap(this.onTable[i], card, this.swapCards)) {
+                console.log('overlap with card on table');
+            }
         }
-      }
-      for (j = 0; j < this.thisHand.length; j++) {
-        if(game.physics.arcade.overlap(this.thisHand[j], card, this.swapCards)) {
-          console.log('overlap with card in this hand');
+        for (j = 0; j < this.thisHand.length; j++) {
+            if (game.physics.arcade.overlap(this.thisHand[j], card, this.swapCards)) {
+                console.log('overlap with card in this hand');
+            }
         }
-      }
-      // snaps card into original position
-      card.position.copyFrom(card.originalPosition);
+        // snaps card into original position
+        card.position.copyFrom(card.originalPosition);
     },
-    swapCards: function(card1, card2){
-      let index1 = _this.deck.indexOf(card1.frame);
-      let index2 = _this.deck.indexOf(card2.frame);
+    swapCards: function (card1, card2) {
+        let c1x = card1.x;
+        let c1y = card1.y;
+        card1.x = card2.x;
+        card1.y = card2.y;
+        card2.x = c1x;
+        card2.y = c1y;
+        // let index1 = _this.deck.indexOf(card1.frame);
+        // let index2 = _this.deck.indexOf(card2.frame);
 
-      // let onTableIndex1 = _this.onTable.indexOf(card1);
-      // let onTableIndex2 = _this.onTable.indexOf(card2);
-      // let thisHandIndex1 = _this.thisHand.indexOf(card1);
-      // let thisHandIndex2 = _this.thisHand.indexOf(card2);
+        // // let onTableIndex1 = _this.onTable.indexOf(card1);
+        // // let onTableIndex2 = _this.onTable.indexOf(card2);
+        // // let thisHandIndex1 = _this.thisHand.indexOf(card1);
+        // // let thisHandIndex2 = _this.thisHand.indexOf(card2);
 
-      let swapped1 = _this.makeCard(index2,card2.x,card2.y);
-      let swapped2 = _this.makeCard(index1,card1.x,card1.y);
+        // let swapped1 = _this.makeCard(index2, card2.x, card2.y);
+        // let swapped2 = _this.makeCard(index1, card1.x, card1.y);
 
-      // if(onTableIndex1 !== -1) {
-      //   _this.onTable[onTableIndex1] = swapped1;
-      // }
-      // else if (onTableIndex2 !== -1) {
-      //   _this.onTable[onTableIndex2] = swapped2;
-      // }
-      // else if (thisHandIndex1 !== -1) {
-      //   _this.thisHand[thisHandIndex1] = swapped1;
-      // }
-      // else if (thisHandIndex2 !== -1) {
-      //   _this.thisHand[thisHandIndex2] = swapped2;
-      // }
+        // // if(onTableIndex1 !== -1) {
+        // //   _this.onTable[onTableIndex1] = swapped1;
+        // // }
+        // // else if (onTableIndex2 !== -1) {
+        // //   _this.onTable[onTableIndex2] = swapped2;
+        // // }
+        // // else if (thisHandIndex1 !== -1) {
+        // //   _this.thisHand[thisHandIndex1] = swapped1;
+        // // }
+        // // else if (thisHandIndex2 !== -1) {
+        // //   _this.thisHand[thisHandIndex2] = swapped2;
+        // // }
 
-      // let pos1 = card1.position.clone();
-      // let pos2 = card2.position.clone();
-      card1.destroy();
-      card2.destroy();
+        // // let pos1 = card1.position.clone();
+        // // let pos2 = card2.position.clone();
+        // card1.destroy();
+        // card2.destroy();
 
-      // TODO: update onTable and thisHand
+        // // TODO: update onTable and thisHand
 
+    },
+    // snaps card back into original position after drag
+    stopDrag: function (sprite1, sprite2) {
+        if (!this.game.physics.arcade.overlap(sprite1, sprite2, function () {
+            sprite1.input.draggable = false;
+            sprite1.position.copyFrom(sprite2.position);
+            sprite1.anchor.setTo(sprite2.anchor.x, sprite2.anchor.y);
+        }));
+        sprite1.position.copyFrom(sprite1.originalPosition);
+    },
+    nextTurn: function () {
+        window.alert('button pressed!');
+        console.log('button pressed!');
     }
 }
 
