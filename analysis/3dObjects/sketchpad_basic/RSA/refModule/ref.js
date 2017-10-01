abb0c116c98e5fc3a05a3e371849422a970d3989
hawkrobe <rxdh@stanford.edu>
2017-09-30 21:00:36 -0700

    Implement RSA model & bayesian data analysis wrapper

---
+++ analysis/3dObjects/sketchpad_basic/RSA/refModule/ref.js
@@ -0,0 +1,102 @@
+var _ = require('lodash');
+var fs = require('fs');
+var babyparse = require('babyparse');
+// var JSONStream = require('JSONStream');
+// var es = require('event-stream');
+
+var getSimilarities = function(name) {
+  return require('./json/' + name + '.json');
+  // fs.createReadStream('./json/' + name + '.json')
+  //   .pipe(JSONStream.parse([render, sketch]))
+  //   .pipe(es.mapSync(function (data) {
+  //     console.log(data);
+  //     return data;
+  //   }));
+};
+
+var getPossibleSketches = function(similarities) {
+  return _.keys(similarities);
+};
+
+
+// console.log(getDistance('cars_07_hatchback_0038.npy',
+// 			'gameID_2848-1779ba4d-c84b-4050-8a1f-e4b733ceb6a7_trial_9.npy',
+// 			{modelVersion: 'out_formatted.json'}));
+
+function readCSV(filename){
+  return babyparse.parse(fs.readFileSync(filename, 'utf8'),
+			 {header:true}).data;
+};
+
+function writeCSV(jsonCSV, filename){
+  fs.writeFileSync(filename, babyparse.unparse(jsonCSV) + '\n');
+}
+
+function appendCSV(jsonCSV, filename){
+  fs.appendFileSync(filename, babyparse.unparse(jsonCSV) + '\n');
+}
+
+var writeERP = function(erp, labels, filename, fixed) {
+  var data = _.filter(erp.support().map(
+   function(v) {
+     var prob = Math.exp(erp.score(v));
+     if (prob > 0.0){
+      if(v.slice(-1) === ".")
+        out = butLast(v);
+      else if (v.slice(-1) === "?")
+        out = butLast(v).split("Is")[1].toLowerCase();
+      else 
+        out = v
+      return labels.concat([out, String(prob.toFixed(fixed))]);
+
+    } else {
+      return [];
+    }
+  }
+  ), function(v) {return v.length > 0;});
+  appendCSV(data, filename);
+};
+
+var supportWriter = function(s, p, handle) {
+  var sLst = _.pairs(s);
+  var l = sLst.length;
+
+  for (var i = 0; i < l; i++) {
+    fs.writeSync(handle, sLst[i].join(',')+','+p+'\n');
+  }
+};
+
+// Note this is highly specific to a single type of erp
+var bayesianErpWriter = function(erp, filePrefix) {
+  var predictiveFile = fs.openSync(filePrefix + "Predictives.csv", 'w');
+  fs.writeSync(predictiveFile, ["condition", "TargetColor","TargetType","Dist1Color","Dist1Type","Dist2Color","Dist2Type",
+				"value", "prob", "MCMCprob"] + '\n');
+
+  var paramFile = fs.openSync(filePrefix + "Params.csv", 'w');
+  fs.writeSync(paramFile, ["parameter", "value", "MCMCprob"] + '\n');
+
+  var supp = erp.support();
+  supp.forEach(function(s) {
+    supportWriter(s.predictive, erp.score(s), predictiveFile);
+    supportWriter(s.params, erp.score(s), paramFile);
+  });
+  fs.closeSync(predictiveFile);
+  fs.closeSync(paramFile);
+  console.log('writing complete.');
+};
+
+var getSubset = function(data, properties) {
+  var matchProps = _.matches(properties);
+  return _.filter(data, matchProps);
+};
+
+var locParse = function(filename) {
+  return babyparse.parse(fs.readFileSync(filename, 'utf8'),
+       {header: true,
+        skipEmptyLines : true}).data;
+};
+
+module.exports = {
+  getSimilarities, getPossibleSketches, getSubset, 
+  bayesianErpWriter, writeERP, writeCSV, readCSV, locParse
+};
