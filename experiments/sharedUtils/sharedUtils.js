7ef877cea667071ffabf8406f07ebcfaf4a8d260
Judy Fan <judithfan@gmail.com>
2017-10-24 22:02:24 -0700

    set db to check to 3dObjects... will need to handle this in a smarter way in future

--- experiments/sharedUtils/sharedUtils.js
+++ experiments/sharedUtils/sharedUtils.js
@@ -25,7 +25,7 @@ var handleInvalidID = function(req, res) {
 var checkPreviousParticipant = function(workerId, callback) {
   var p = {'workerId': workerId};
   var postData = {
-    dbname: 'basicLevel',
+    dbname: '3dObjects',
     query: p,
     projection: {'_id': 1}
   };
