9b86f428cb601d8e9f72525fe447a0a752fdc48d
Judy Fan <judithfan@gmail.com>
2017-10-31 10:36:46 -0700

    set up for running bda on photo-augmented-2 distances

--- analysis/3dObjects/sketchpad_basic/RSA/refModule/ref.js
+++ analysis/3dObjects/sketchpad_basic/RSA/refModule/ref.js
@@ -6,7 +6,7 @@ var babyparse = require('babyparse');
 
 var getSimilarities = function(name) {
   return {
-    'strict-mid-pragmatics' : require('./json/strict-similarity-pragmatics-fixedpose_conv4_2.json'),
+    'strict-mid-pragmatics-augmented2' : require('./json/strict-similarity-pragmatics-fixedpose-augmented2_conv4_2.json'),
     'strict-mid-pragmatics-augmented' : require('./json/strict-similarity-pragmatics-fixedpose-augmented_conv4_2.json'),
     'nonstrict-high-sketchy' : require('./json/nonstrict-similarity-sketchy-fixedpose_fc7.json')
   };
