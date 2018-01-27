f90729c2a4ed07bac9635baa1c715d1bdc8627d4
Bill McDowell <forkunited@gmail.com>
2018-01-27 10:56:59 -0800

    Added code to generate random trials

---
+++ experiments/shapeGrids/syn/make_trials.js
@@ -0,0 +1,72 @@
+var ROUNDS_PER_GAME = 20;
+var NUM_GAMES = process.argv[2];
+var OUTPUT_DIRECTORY = process.argv[3];
+
+_ = require('underscore');
+assert = require('assert');
+jsonfile = require('jsonfile');
+mkdirp = require('mkdirp');
+fs = require('fs');
+utils = require('../../sharedUtils/sharedUtils.js');
+trial = require('../trial.js');
+
+/**
+ * Shuffles array in place.
+ * From https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+}
+
+function makeGameTrials(numRounds) {
+  var trialList = [];
+  for (var i = 0; i < numRounds; i++) {
+    trialList.push(trial.makeRandom(i, numRounds)); // i/2
+  };
+
+  shuffle(trialList);
+
+  return trialList;
+};
+
+function makeDataPoint(gameId, roundNum, roundObj) {
+  var stateDataPoint = {
+    type : "StateGrid",
+    gameid: gameId,
+    time : Date.now(),
+    roundNum : roundNum
+  };
+
+  var trialDims = trial.getDimensionValues(roundObj);
+  var dimNames = trial.getDimensionNames();
+  for (var i = 0; i < dimNames.length; i++) {
+    stateDataPoint[dimNames[i]] = trialDims[i];
+  }
+
+  return stateDataPoint;
+}
+
+mkdirp.sync(OUTPUT_DIRECTORY, err => {if (err) console.error(err);});
+
+for (var i = 0; i < NUM_GAMES; i++) {
+  var trials = makeGameTrials(ROUNDS_PER_GAME);
+  var gameDataStr = _.keys(makeDataPoint(i, 0, trials[0])).join('\t') + '\n';
+
+  for (var j = 0; j < ROUNDS_PER_GAME; j++) {
+    var dataPoint = makeDataPoint(i, j, trials[j]);
+    gameDataStr += _.values(dataPoint).join('\t') + "\n";
+  }
+
+  var fileName =  i + ".csv";
+  var filePath = [OUTPUT_DIRECTORY, fileName].join('/');
+  var stream = fs.createWriteStream(filePath);
+  stream.write(gameDataStr, err => {if(err) throw err;});
+  stream.end();
+}
